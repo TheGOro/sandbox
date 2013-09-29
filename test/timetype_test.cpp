57c30aa0da402cd877aecdcfb77106bb97578148
Gabor Orosz <goro@goro.hu>
2013-09-29 19:50:44 +0200

    Refines TimeType tests

--- test/timetype_test.cpp
+++ test/timetype_test.cpp
@@ -1,44 +1,62 @@
 #include <gtest/gtest.h>
 #include "../src/timetype.h"
 
-TEST(TimeType, Degree0) {
-    TimeType t1(0,  0);
-    EXPECT_DOUBLE_EQ(0.0, t1.getDegree());
-    
-    TimeType t2(5, 25);
-    EXPECT_DOUBLE_EQ(0.0, t2.getDegree());
-    
-    TimeType t3(10, 50);
-    EXPECT_DOUBLE_EQ(0.0, t3.getDegree());
-    
-    TimeType t4(13, 05);
-    EXPECT_DOUBLE_EQ(0.0, t4.getDegree());
+TEST(TimeType, DegreeOfMidnight) {
+    TimeType t(0,  0);
+    EXPECT_DOUBLE_EQ(0.0, t.getDegree());
 }
 
-TEST(TimeType, Degree90) {
-    TimeType t1(13, 20);
-    EXPECT_DOUBLE_EQ(90.0, t1.getDegree());
-    
-    TimeType t2( 9, 30);
-    EXPECT_DOUBLE_EQ(90.0, t2.getDegree());
-    
-    TimeType t3(22,  5);
-    EXPECT_DOUBLE_EQ(90.0, t3.getDegree());
-    
-    TimeType t4( 4, 25);
-    EXPECT_DOUBLE_EQ(90.0, t4.getDegree());
+TEST(TimeType, DegreeOfFiveToHalfPastFive) {
+    TimeType t(5, 25);
+    EXPECT_DOUBLE_EQ(0.0, t.getDegree());
 }
 
-TEST(TimeType, Degree180) {
-    TimeType halfPastNoon(12, 30);
-    EXPECT_DOUBLE_EQ(180.0, halfPastNoon.getDegree());
-    
-    TimeType quarterToFour(3, 45);
-    EXPECT_DOUBLE_EQ(180.0, quarterToFour.getDegree());
-    
-    TimeType forteenForty(14, 40);
-    EXPECT_DOUBLE_EQ(180.0, forteenForty.getDegree());
-    
-    TimeType elevenTwentyFive(11, 25);
-    EXPECT_DOUBLE_EQ(180.0, elevenTwentyFive.getDegree());
+TEST(TimeType, DegreeOfTenToEleven) {
+    TimeType t(10, 50);
+    EXPECT_DOUBLE_EQ(0.0, t.getDegree());
+}
+
+TEST(TimeType, DegreeOfFivePastThirteen) {
+    TimeType t(13, 05);
+    EXPECT_DOUBLE_EQ(0.0, t.getDegree());
+}
+
+TEST(TimeType, DegreeOfTenToHalfPastThirteen) {
+    TimeType t(13, 20);
+    EXPECT_DOUBLE_EQ(90.0, t.getDegree());
+}
+
+TEST(TimeType, DegreeOfHalfPastNine) {
+    TimeType t( 9, 30);
+    EXPECT_DOUBLE_EQ(90.0, t.getDegree());
+}
+
+TEST(TimeType, DegreeOfFivePastTwentytwo) {
+    TimeType t(22,  5);
+    EXPECT_DOUBLE_EQ(90.0, t.getDegree());
+}
+
+TEST(TimeType, DegreeOfFiveToHalfPastFour) {    
+    TimeType t( 4, 25);
+    EXPECT_DOUBLE_EQ(90.0, t.getDegree());
+}
+
+TEST(TimeType, DegreeOfHalfPastNoon) {
+    TimeType t(12, 30);
+    EXPECT_DOUBLE_EQ(180.0, t.getDegree());
+}
+
+TEST(TimeType, DegreeOfQuarterToFour) {
+    TimeType t(3, 45);
+    EXPECT_DOUBLE_EQ(180.0, t.getDegree());
+}
+
+TEST(TimeType, DegreeOfTenPastHalfPastFourteen) {    
+    TimeType t(14, 40);
+    EXPECT_DOUBLE_EQ(180.0, t.getDegree());
+}
+
+TEST(TimeType, DegreeOfFiveToHalfPastEleven) {
+    TimeType t(11, 25);
+    EXPECT_DOUBLE_EQ(180.0, t.getDegree());
 }
